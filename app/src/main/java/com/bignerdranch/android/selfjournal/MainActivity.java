03d06d0765be8d46b1d79833c58b3bc29dcbafd5
Djumabaevs <djumabaevb@gmail.com>
2021-04-28 00:48:40 +0600

    add login activity

--- app/src/main/java/com/bignerdranch/android/selfjournal/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/selfjournal/MainActivity.java
@@ -3,12 +3,20 @@ package com.bignerdranch.android.selfjournal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button getStartedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getStartedButton = findViewById(R.id.start_button);
+
+        getStartedButton.setOnClickListener(view -> {
+            //go to login activity
+
+        });
     }
 }

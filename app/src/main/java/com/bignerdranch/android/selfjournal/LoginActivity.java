115f9aec572d8259cba1d515679cd6c17dc3a3c2
Djumabaevs <djumabaevb@gmail.com>
2021-04-28 20:59:48 +0600

    another intent

--- app/src/main/java/com/bignerdranch/android/selfjournal/LoginActivity.java
+++ app/src/main/java/com/bignerdranch/android/selfjournal/LoginActivity.java
@@ -2,13 +2,24 @@ package com.bignerdranch.android.selfjournal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button loginButton;
+    private Button createAcctButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginButton = findViewById(R.id.email_sign_in_button);
+        createAcctButton = findViewById(R.id.create_account_button_login);
+
+        createAcctButton.setOnClickListener(view -> {
+            startActivity(new Intent(LoginActivity.this, CreateAccountActivity.class));
+        });
+
     }
 }

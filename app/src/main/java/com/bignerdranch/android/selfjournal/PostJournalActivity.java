05a2727525592daa39958c1179119c44257d407b
Djumabaevs <djumabaevb@gmail.com>
2021-04-30 23:54:23 +0600

    authStateListener method

--- app/src/main/java/com/bignerdranch/android/selfjournal/PostJournalActivity.java
+++ app/src/main/java/com/bignerdranch/android/selfjournal/PostJournalActivity.java
@@ -1,17 +1,34 @@
 package com.bignerdranch.android.selfjournal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 
 import com.bignerdranch.android.selfjournal.databinding.ActivityPostJournalBinding;
+import com.bignerdranch.android.selfjournal.util.JournalApi;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.StorageReference;
 
-public class PostJournalActivity extends AppCompatActivity {
+public class PostJournalActivity extends AppCompatActivity implements View.OnClickListener {
     private ActivityPostJournalBinding binding;
 
     private String currentUserId;
     private String currentUsername;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser user;
+
+    //connection to Firestore
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private StorageReference storageReference;
+
+    private CollectionReference collectionReference = db.collection("Journal");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +38,22 @@ public class PostJournalActivity extends AppCompatActivity {
         variable = this.binding;
         setContentView((View)variable.getRoot());
 
+        firebaseAuth = FirebaseAuth.getInstance();
 
+        binding.postCameraButton.setOnClickListener(this);
+        binding.postSaveJournalButton.setOnClickListener(this);
 
+        if(JournalApi.getInstance() != null) {
+            currentUserId = JournalApi.getInstance().getUserId();
+            currentUsername = JournalApi.getInstance().getUsername();
 
+            binding.postUsernameTextview.setText(currentUsername);
+        }
+
+        authStateListener = firebaseAuth -> {
+            user = firebaseAuth.getCurrentUser();
+
+        };
 
 
 
@@ -33,5 +63,17 @@ public class PostJournalActivity extends AppCompatActivity {
             String username = bundle.getString("username");
             String userId = bundle.getString("userId");
         }*/
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch ((v.getId())) {
+            case R.id.post_save_journal_button:
+
+                break;
+            case R.id.post_camera_button:
+
+                break;
+        }
     }
 }

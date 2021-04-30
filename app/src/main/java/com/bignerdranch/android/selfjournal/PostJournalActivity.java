e0e12829e8e6da0f579306bcc44b82c88ec6366a
Djumabaevs <djumabaevb@gmail.com>
2021-05-01 00:30:50 +0600

    imageUri in onActivityResilt()

--- app/src/main/java/com/bignerdranch/android/selfjournal/PostJournalActivity.java
+++ app/src/main/java/com/bignerdranch/android/selfjournal/PostJournalActivity.java
@@ -1,8 +1,11 @@
 package com.bignerdranch.android.selfjournal;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -15,6 +18,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.StorageReference;
 
 public class PostJournalActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int GALLERY_CODE = 1;
     private ActivityPostJournalBinding binding;
 
     private String currentUserId;
@@ -29,6 +33,7 @@ public class PostJournalActivity extends AppCompatActivity implements View.OnCli
     private StorageReference storageReference;
 
     private CollectionReference collectionReference = db.collection("Journal");
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +57,11 @@ public class PostJournalActivity extends AppCompatActivity implements View.OnCli
 
         authStateListener = firebaseAuth -> {
             user = firebaseAuth.getCurrentUser();
+            if(user != null) {
 
+            } else {
+
+            }
         };
 
 
@@ -72,8 +81,35 @@ public class PostJournalActivity extends AppCompatActivity implements View.OnCli
 
                 break;
             case R.id.post_camera_button:
-
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_CODE);
                 break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GALLERY_CODE && resultCode == RESULT_OK) {
+            if(data != null) {
+                imageUri = data.getData();
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        user = firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(firebaseAuth != null) {
+            firebaseAuth.removeAuthStateListener(authStateListener);
         }
     }
 }

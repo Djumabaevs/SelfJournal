727abfa9c9c110abff9f37f5dc2f0c7fc4c33d9b
Djumabaevs <djumabaevb@gmail.com>
2021-05-03 09:54:39 +0600

    onCreateOptionsMenu()

--- app/src/main/java/com/bignerdranch/android/selfjournal/JournalListActivity.java
+++ app/src/main/java/com/bignerdranch/android/selfjournal/JournalListActivity.java
@@ -3,6 +3,7 @@ package com.bignerdranch.android.selfjournal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class JournalListActivity extends AppCompatActivity {
 
@@ -11,4 +12,12 @@ public class JournalListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_list);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
 }

cca9c8cdeb09a94e56bcc55536186335bbe1e14e
Djumabaevs <djumabaevb@gmail.com>
2021-05-08 14:41:32 +0600

    picasso added into onBindViewHolder()

--- app/src/main/java/com/bignerdranch/android/selfjournal/ui/JournalRecyclerAdapter.java
+++ app/src/main/java/com/bignerdranch/android/selfjournal/ui/JournalRecyclerAdapter.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.selfjournal.ui;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bignerdranch.android.selfjournal.R;
 import com.bignerdranch.android.selfjournal.model.Journal;
 
 import java.util.List;
@@ -25,11 +27,20 @@ public class JournalRecyclerAdapter extends RecyclerView.Adapter<JournalRecycler
     @NonNull
     @Override
     public JournalRecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context)
+                .inflate(R.layout.journal_row, parent, false);
+
+        return new ViewHolder(view, context);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull JournalRecyclerAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull JournalRecyclerAdapter.ViewHolder viewHolder, int position) {
+        String imageUrl;
+        Journal journal = journalList.get(position);
+        viewHolder.title.setText(journal.getTitle());
+        viewHolder.thoughts.setText(journal.getThought());
+//        viewHolder.name.setText(journal.getUserName());
+
 
     }
 
@@ -41,10 +52,18 @@ public class JournalRecyclerAdapter extends RecyclerView.Adapter<JournalRecycler
     public class ViewHolder extends RecyclerView.ViewHolder{
         public TextView title, thoughts, dateAdded, name;
         public ImageView image;
-        
+        String userId;
+        String username;
 
-        public ViewHolder(@NonNull View itemView) {
+        public ViewHolder(@NonNull View itemView, Context ctx) {
             super(itemView);
+            context = ctx;
+
+            title = itemView.findViewById(R.id.journal_title_list);
+            thoughts = itemView.findViewById(R.id.journal_thought_list);
+            dateAdded = itemView.findViewById(R.id.journal_timestamp_list);
+            image = itemView.findViewById(R.id.journal_image_list);
+//            name = itemView.findViewById(R.id.username_account);
         }
     }
 }
